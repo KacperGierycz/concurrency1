8ba3516b4e914f3e62daea58403314bf1b3405e4
Kacper <kacpergierycz@gmail.com>
2021-07-17 12:39:42 +0200

    sheduled reporter

--- concurrency1/src/concurrency1/H_PrimeNumbersWithThreadPool.java
+++ concurrency1/src/concurrency1/H_PrimeNumbersWithThreadPool.java
@@ -3,13 +3,26 @@ package concurrency1;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class H_PrimeNumbersWithThreadPool {
 	
 
 		public static void main(String[] args) {
 			
+			ScheduledExecutorService sheduledExecutor=Executors.newScheduledThreadPool(1);
+			Runnable reporterRunnable= ()->{
+				System.out.println("running raporter");
+			};
+
+			
+			sheduledExecutor.scheduleAtFixedRate(reporterRunnable, 1, 5, TimeUnit.SECONDS);
+			
+			
 			ExecutorService executorService=Executors.newFixedThreadPool(3);
+		//	ExecutorService executorService=Executors.newCachedThredPool();
+			
 
 			int number;
 			
